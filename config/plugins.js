213b655b4cdc8b35ebfab170a67db03cbc55d16d
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-01-12 17:32:55 +0800

    Try Cloudinary and midware

--- config/plugins.js
+++ config/plugins.js
@@ -51,4 +51,20 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
+  //cloudinary
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
 });
