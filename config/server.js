fc309649977d3f8e1d98bb34710ce440f3f0dc24
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-01-12 18:05:25 +0800

    Remove baseURL fix to redirect Cloudinary

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST'),
   port: env.int('PORT'),
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  // url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
