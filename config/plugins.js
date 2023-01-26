644b7b82e04b65ed43753e4019739beb1a9c4eff
Paulo <99748141+pow-carriera@users.noreply.github.com>
2023-01-26 20:10:51 +0800

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -56,18 +56,18 @@ module.exports = ({ env }) => ({
   },
 
   //cloudinary
-//   upload: {
-//     config: {
-//       provider: 'cloudinary',
-//       providerOptions: {
-//         cloud_name: env('CLOUDINARY_NAME'),
-//         api_key: env('CLOUDINARY_KEY'),
-//         api_secret: env('CLOUDINARY_SECRET'),
-//       },
-//       actionOptions: {
-//         upload: {},
-//         delete: {},
-//       },
-//     },
-//   },
+   upload: {
+     config: {
+       provider: 'cloudinary',
+       providerOptions: {
+         cloud_name: env('CLOUDINARY_NAME'),
+         api_key: env('CLOUDINARY_KEY'),
+         api_secret: env('CLOUDINARY_SECRET'),
+       },
+       actionOptions: {
+         upload: {},
+         delete: {},
+       },
+     },
+   },
 });
