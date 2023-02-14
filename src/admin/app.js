0066c03a33e3e6f74552973ae2a46236efe86e2b
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-02-14 21:21:39 +0800

    Stage some style changes

---
+++ src/admin/app.js
@@ -0,0 +1,20 @@
+// const config = {
+//   translations: {
+//     en: {
+//       'app.components.LeftMenu.navbrand.title': 'HerbalAid',
+//       'app.components.LeftMenu.navbrand.title': 'Administrator Panel'
+//     }
+//   },
+//   auth: {
+//     logo: "/extension/logo-icon.png"
+//   }
+// };
+
+// const bootstrap = (app) => {
+//   console.log(app);
+// };
+
+// export default {
+//   config,
+//   bootstrap,
+// };
